d51e6952531d977ac45d8c1bb0ef5c2e1ee4dd75
Jimmy John <jimmyislive@gmail.com>
2019-07-26 21:40:09 -0700

    cloudfront in TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,22 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
+
+#provider "aws" {
+#  alias      = "acm_east_provider"
+#  access_key = var.aws_access_key
+#  secret_key = var.aws_secret_key
+#  region     = "us-east-1"
+#}
 
 resource "aws_s3_bucket" "static_website_bucket" {
   bucket = "${var.namespace}-demos3staticweb"
-  acl = "public-read"
+  acl    = "public-read"
 
-  tags {
-    Name = "DemoAWSS3StaticWeb"
+  tags = {
+    Name        = "DemoAWSS3StaticWeb"
     Environment = "production"
   }
 
@@ -28,7 +35,56 @@ resource "aws_s3_bucket" "static_website_bucket" {
 }
 EOF
 
+
   website {
     index_document = "index.html"
+  }
+}
+
+
+resource "aws_cloudfront_distribution" "demos3staticweb_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.static_website_bucket.bucket_regional_domain_name}"
+    origin_id   = "S3-${var.namespace}-demos3staticweb"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${var.namespace}-demos3staticweb"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  tags = {
+    Environment = "production"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,14 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
+
 variable "aws_region" {
   default = "us-west-2"
 }
 
 # Use the namespace to customize your bucketname e.g. username
-variable "namespace" {}
+variable "namespace" {
+}
+
